8eccda2ad60a1247fd720b28d761a61e9c27c33c
fernandicogeo <fernandico.geovardo01@gmail.com>
2024-02-17 14:33:41 +0700

    Midtrans sudah berhasil

--- resources/views/user/payment.blade.php
+++ resources/views/user/payment.blade.php
@@ -7,7 +7,7 @@
 @section('isi')
   <!-- @TODO: replace SET_YOUR_CLIENT_KEY_HERE with your client key -->
   <script type="text/javascript"
-		src="https://app.stg.midtrans.com/snap/snap.js"
+  src="https://app.sandbox.midtrans.com/snap/snap.js"
     data-client-key="{{ config('midtrans.client_key') }}"></script>
   <!-- Note: replace with src="https://app.midtrans.com/snap/snap.js" for Production environment -->
 
